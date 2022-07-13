6877009979360ab95d2b8955fce6d4e4e957dbba
lucasplr <lucasriffel55@gmail.com>
2022-07-13 16:37:32 -0300

    cont

--- localSystem/main.py
+++ localSystem/main.py
@@ -28,15 +28,18 @@ img_0 = img_0.resize((65, 65), Image.Resampling.LANCZOS)
 img_0 = ImageTk.PhotoImage(img_0)
 
 icon = Label(f_1, image=img_0, relief=FLAT)
-icon.place(x=220, y=20)
+icon.place(x=220, y=45)
 
 
 #labels
 
 l_1 = Label(f_1, text='Login de Usuário', relief=FLAT, bg=cor3, fg=cor5, font=('Ivy 20 bold'))
-l_1.place(x=140, y=120)
+l_1.place(x=140, y=140)
 
+#entrys
 
+e_1 = Entry(f_1, relief=GROOVE, bg=cor1, fg=cor2, width=25)
+e_1.place(x=215, y=200)
 
 
 wd.mainloop()
