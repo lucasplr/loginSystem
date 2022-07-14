8e32a1a98b85070cb3921794698d7068406777d4
lucasplr <lucasriffel55@gmail.com>
2022-07-13 22:45:19 -0300

    entrys

--- localSystem/main.py
+++ localSystem/main.py
@@ -32,14 +32,19 @@ img_1 = Image.open('img/email.png')
 img_1 = img_1.resize((20, 20), Image.Resampling.LANCZOS)
 img_1 = ImageTk.PhotoImage(img_1)
 
+img_2 = Image.open('img/password.png')
+img_2 = img_2.resize((20, 20), Image.Resampling.LANCZOS)
+img_2 = ImageTk.PhotoImage(img_2)
 
 
 icon = Label(f_1, image=img_0, relief=FLAT)
 icon.place(x=220, y=45)
 
 icon2 = Label(f_1, image=img_1, relief=FLAT)
-icon2.place(x=155, y=197)
+icon2.place(x=155, y=215)
 
+icon3 = Label(f_1, image=img_2, relief=FLAT)
+icon3.place(x=155, y=277)
 
 #labels
 
@@ -49,7 +54,9 @@ l_1.place(x=140, y=140)
 #entrys
 
 e_1 = Entry(f_1, relief=GROOVE, bg=cor1, fg=cor2, width=25)
-e_1.place(x=200, y=200)
+e_1.place(x=200, y=218)
 
+e_2 = Entry(f_1, relief=GROOVE, bg=cor1, fg=cor2, width=25)
+e_2.place(x=200, y=280)
 
 wd.mainloop()
